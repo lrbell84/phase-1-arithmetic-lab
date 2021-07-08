ccb20c3c9186d6fba6e18343f84b0572169514e3
Lydia Bell <lrbell84@gmail.com>
2021-07-08 21:59:54 +0000

    commit tree e3077a7d000e38112d7030a78b12002ca8aec931

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random() * 100);
+
+const num3 = 28;
+const num4 = 6;
+
+let mod = (num3 % num4);
+
+let max = Math.max(5, 10, 15, 20);
+Math.max(5, 10, 15, 20);
